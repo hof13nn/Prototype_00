3546927e2d1e3b3b2046397e75ed4827e26e89fc
hof13nn <45291182+hof13nn@users.noreply.github.com>
2024-10-17 11:16:52 +0300

    02. Project creation

---
+++ Source/Prototype_00/Prototype_00.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Prototype_00.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Prototype_00, "Prototype_00" );
